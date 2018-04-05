50329a833e05a1a268867b7127bc614fc58aac32
awunderground <aaronrwilliams100@gmail.com>
2018-04-05 18:45:54 -0400

    Switch README from .md to .Rmd

--- R/urban_logo_text.R
+++ R/urban_logo_text.R
@@ -6,16 +6,16 @@
 #' @import gridExtra
 #' @md
 #' @export
-caption <- function() {
+urban_logo_text <- function() {
   grobTree(
   gp = gpar(fontsize = 7, hjust = 1),
-  textGrob(label = "I N S T I T U T E",
+  textGrob(label = " I N S T I T U T E",
            name = "caption1",
            x = unit(1, "npc"),
            y = unit(0, "npc"),
            hjust = 1,
            vjust = 0),
-  textGrob(label = "U R B A N",
+  textGrob(label = "U R B A N ",
            x = unit(1, "npc") - grobWidth("caption1") - unit(0.001, "lines"),
            y = unit(0, "npc"),
            hjust = 1,
