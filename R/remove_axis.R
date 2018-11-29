a28dd7e42b993e4fe97f3b52af67b6cbd63e0eb7
awunderground <aaronrwilliams100@gmail.com>
2018-11-29 15:52:27 -0500

    Add flip = argument to remove_axis() to handle coord_flip()

--- R/remove_axis.R
+++ R/remove_axis.R
@@ -3,22 +3,43 @@
 #' Removes axes and axes labels
 #'
 #' @param axis Axis to drop. "x", "y", or "both"
+#' @param flip TRUE/FALSE is `coord_flip()` is used
 #'
 #' @md
 #' @export
-remove_axis <- function(axis = "y") {
+remove_axis <- function(axis = "y", flip = FALSE) {
 
-  if (axis == "x") {
+  if (axis == "x" && flip == FALSE) {
 
     ggplot2::theme(panel.grid.major.x = ggplot2::element_blank(),
                    axis.text.x = ggplot2::element_blank(),
                    axis.line.x = ggplot2::element_blank())
 
-  } else if (axis == "y") {
+  } else if (axis == "x" && flip == TRUE) {
+
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                     axis.text.x = ggplot2::element_blank(),
+                     axis.line.x = ggplot2::element_blank(),
+                     axis.ticks = ggplot2::element_blank(),
+                     axis.line.y = ggplot2::element_line(colour = NULL,
+                                                         size = NULL,
+                                                         linetype = NULL,
+                                                         lineend = NULL))
+
+  } else if (axis == "y" && flip == FALSE) {
 
     ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_blank(),
-                   axis.line.y = ggplot2::element_blank())
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank())
+
+  } else if (axis == "y" && flip == TRUE) {
+
+    ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_blank(),
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank(),
+                   axis.title.y = ggplot2::element_blank())
 
   } else if (axis == "both") {
 
@@ -27,12 +48,13 @@ remove_axis <- function(axis = "y") {
                    axis.line.x = ggplot2::element_blank(),
                    panel.grid.major.y = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_blank(),
-                   axis.line.y = ggplot2::element_blank())
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.ticks = ggplot2::element_blank())
 
   } else {
 
-    stop('Invalid "axis" argument. Valid arguments are: ',
-              '"x", "y", and "both"',
+    stop('Invalid "axis" or "flip" argument. Valid arguments are: ',
+              '"x", "y", and "both", or TRUE/FALSE',
               call. = FALSE)
   }
 }
