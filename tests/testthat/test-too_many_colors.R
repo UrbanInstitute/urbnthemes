5e33d48696c2ee0cd87cb20fe6373d192325e7bc
Ajjit Narayanan <ajjit123@gmail.com>
2021-06-21 12:08:35 -0400

    Try printing ggplots inside tests

--- tests/testthat/test-too_many_colors.R
+++ tests/testthat/test-too_many_colors.R
@@ -4,38 +4,48 @@ test_that("Including more than 8 categories for color and fill throws an error "
 
   set_urbn_defaults(style = "print")
 
-  expect_error(
-    ggplot2::ggplot(
-      ggplot2::mpg,
-      ggplot2::aes(cty, hwy, color = manufacturer)
-    ) +
-      urbnthemes::geom_point()
-  )
 
-  expect_success(
-    ggplot2::ggplot(
-      ggplot2::mpg,
-      ggplot2::aes(cty, hwy, color = manufacturer)
-    ) +
-      geom_point() +
-      ggplot2::scale_color_discrete()
-  )
+  broken_plot_too_many_colors <- ggplot2::ggplot(
+    ggplot2::mpg,
+    ggplot2::aes(cty, hwy, color = manufacturer)
+  ) +
+    urbnthemes::geom_point()
 
   expect_error(
-    ggplot2::ggplot(
-      ggplot2::mpg,
-      ggplot2::aes(manufacturer, hwy, fill = manufacturer)
-    ) +
-      geom_col()
+    print(broken_plot_too_many_colors)
   )
 
+  working_plot <- ggplot2::ggplot(
+    ggplot2::mpg,
+    ggplot2::aes(cty, hwy, color = manufacturer)
+  ) +
+    geom_point() +
+    ggplot2::scale_color_discrete()
+
+
   expect_success(
-    ggplot2::ggplot(
-      ggplot2::mpg,
-      ggplot2::aes(manufacturer, hwy, fill = manufacturer)
-    ) +
-      geom_col() +
-      ggplot2::scale_fill_discrete()
+    print(working_plot)
+  )
+
+  broken_plot2_too_many_colors <- ggplot2::ggplot(
+    ggplot2::mpg,
+    ggplot2::aes(manufacturer, hwy, fill = manufacturer)
+  ) +
+    geom_col()
+
+  expect_error(
+    print(broken_plot2_too_many_colors)
+  )
+
+  working_plot2 <- ggplot2::ggplot(
+    ggplot2::mpg,
+    ggplot2::aes(manufacturer, hwy, fill = manufacturer)
+  ) +
+    geom_col() +
+    ggplot2::scale_fill_discrete()
+
+  expect_success(
+    print(working_plot2)
   )
 
 })
